cd882c90ca3fdee82be92afa11c5f78e74e1d4cd
vishesh <vishesh.gubrani@gmail.com>
2026-01-12 20:29:33 +0530

    added docker file

--- server/src/lib/database.ts
+++ server/src/lib/database.ts
@@ -1,7 +1,9 @@
-import { neon } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 import * as schema from '../db/schema'
 
-const databaseUrl = process.env.DATABASE_URL!
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+})
 
-export const db = drizzle(databaseUrl, { schema })
+export const db = drizzle(pool, { schema })
