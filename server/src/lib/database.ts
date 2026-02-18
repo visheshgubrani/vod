5b6105085f195147d6ea4c048c66481615a395e8
vishesh <vishesh.gubrani@gmail.com>
2026-02-18 20:49:56 +0530

    added session catching and using neon serverless now to improve api responses

--- server/src/lib/database.ts
+++ server/src/lib/database.ts
@@ -1,9 +1,8 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
+import { drizzle } from 'drizzle-orm/neon-http'
+import { neon } from '@neondatabase/serverless'
 import * as schema from '../db/schema'
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-})
+const sql = neon(process.env.DATABASE_URL!)
 
-export const db = drizzle(pool, { schema })
+export const db = drizzle(sql, { schema })
+
