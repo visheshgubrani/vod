d44a1065d509118f1cf848eec3d15fd2a540329d
vishesh <vishesh.gubrani@gmail.com>
2026-02-23 14:43:23 +0530

    added pg pool in the db

--- server/src/lib/database.ts
+++ server/src/lib/database.ts
@@ -1,8 +1,10 @@
-import { drizzle } from 'drizzle-orm/neon-http'
-import { neon } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 import * as schema from '../db/schema'
 
-const sql = neon(process.env.DATABASE_URL!)
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
 
-export const db = drizzle(sql, { schema })
+export const db = drizzle(pool, { schema })
 
